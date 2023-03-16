a4fc3481de7c5c499ccf3aeb5ce3fd061ac1d40c
mensahedmund <mensahedmund0@gmail.com>
2023-03-16 08:34:12 -0700

    4-puts.c3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-    puts("\"Progrmaing is like building a multilingual puzzle");
-    return (0);
-} 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
